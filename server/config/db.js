80b8c78d3ecbc5268c50a3f158c016ba80f537b4
chatluongno1 <cbfrose123is@gmail.com>
2026-06-04 18:00:16 +0700

    fix aiven mysql ssl

--- server/config/db.js
+++ server/config/db.js
@@ -14,6 +14,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
